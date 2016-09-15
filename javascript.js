2d56c7290444d6363a32759f1be324360ec61e21
Peter-32 <peter@impactradius.com>
2016-09-14 19:18:37 -0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+/* window.onload = function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        alert("Yeah!");
+    } else {
+        // jQuery is not loaded
+        alert("Doesn't Work");
+    }
+}
+
+$('.carousel-control').click(function (e) {
+  e.preventDefault();
+});
+$(function(){
+      alert("My First Jquery Test");
+   }); */
